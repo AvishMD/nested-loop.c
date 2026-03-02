90f98d7811ece8e26e6dd100e720a536dd1728d9
AvishMD <avishmd2007@gmail.com>
2026-03-02 14:14:10 +0530

    Create p16.c

---
+++ p16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--) {
+        for(int j=1;j<=2*n-1;j++) {
+            if(j<=(n-i) || j>(n+i-1))
+                printf(" ");
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
